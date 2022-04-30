0628069c4e32b2d9f469c8a0db39e70e14e5c78f
Henintsoa Harinoro <48785203+HenintsoaHARINORO@users.noreply.github.com>
2022-04-30 12:01:53 +0300

    Tasks

--- 2.2.py
+++ 2.2.py
@@ -4,10 +4,10 @@ for i in range(n):
     num = int(input())
     number.append(num)
 print(number)
-min = number[0]
+max = number[0]
 for i in range(len(number)):
-   if number[i] <= min:
-       min = number[i]
+   if number[i] >= max:
+       max = number[i]
 
-print(min)
 
+print(max)
