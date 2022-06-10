6247ae3810344b2703d66ef4d29dbc4d5255c48d
Henintsoa Harinoro <48785203+HenintsoaHARINORO@users.noreply.github.com>
2022-06-10 13:51:28 +0300

    Functions

--- square3.py
+++ square3.py
@@ -4,28 +4,26 @@ wn = turtle.Screen()
 t = turtle.Turtle()
 t.shape('turtle')
 t.width(3)
-t.color('red')
-def sq(size):
-    t.forward(size)
-    t.left(90)
 
-    t.forward(size)
-
-    t.left(90)
-
+def sq(size,col):
+    t.color(col)
     t.forward(size)
     t.left(90)
     t.forward(size)
-def sqin(size):
-    sq(size)
+    t.left(90)
+    t.forward(size)
+    t.left(90)
+    t.forward(size)
+def sqin(size,col1,col2):
+    sq(size,col1)
     t.left(90)
     t.penup()
     t.forward(size/2)
     t.left(45)
     t.pendown()
-    sq(size*math.sqrt(2)/2)
+    sq(size*math.sqrt(2)/2, col2)
 
-sqin(100)
+sqin(100,'blue', 'black')
 
 
 wn.exitonclick()
